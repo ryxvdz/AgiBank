3b7c43a8a4d650b92935899cd8f8ed9e7336ee23
Ryan Dias <ryanvicd2112@gmail.com>
2025-08-13 09:32:21 -0300

    Subindo exercicio de Matriz

--- src/br/com/devsdoagi/Exerc01/Matriz.java
+++ src/br/com/devsdoagi/Exerc01/Matriz.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Matriz {
 
-    void preencherMatriz(double [] [] matriz){
+    public static void preencherMatriz(double [] [] matriz){
         Scanner sc = new Scanner(System.in);
 // l = Linha // c = Coluna
         for (int l= 0; l< matriz.length; l++){
@@ -16,16 +16,27 @@ public class Matriz {
     }
 
 
-    void calcularSomaTotal(double [][] matriz) {
+    public static double calcularSomaTotal(double [][] matriz) {
 
+        double soma = 0;
         for (int l=0; l< matriz.length; l++){
             for (int c= 0; c < matriz[l].length; c++){
-
+                soma += matriz[l][c];
             }
+
         }
+        return soma;
     }
 
     public static void main(String[] args) {
+
+        double[][] matriz = new double[2][2];
+        double total;
+
+        preencherMatriz(matriz);
+        total = calcularSomaTotal(matriz);
+
+        System.out.println("A soma é: " + total);
 
 
     }
